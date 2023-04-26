cef3899e59184be68f8e9775c34e9df2cbb9977d
BogdanTheGeek <bogdan.ionescu.work@gmail.com>
2023-04-26 21:11:25 +0100

    update example

--- main.c
+++ main.c
@@ -17,63 +17,55 @@ int main(int argc, char *argv[])
 
 // Buffer size should include the null terminator
 #if CONFIG_KJSON_SMALLEST
-   char root[118] = {0};
+   char root[204] = {0};
 #else
-   char root[169] = {0};
+   char root[261] = {0};
 #endif
 
-   kjson_t json = {
-       .root = root,
-       .rootSize = sizeof(root),
-       .tail = root,
-       .size = 0,
-       .depth = 0,
-       .newLine = "\n\r",
-       .truncated = false,
-   };
+   kjson_t json = KJSON_INITIALISE(root);
    kjson_t *jsonHandle = &json;
 
-#if TEST
-   int count = 1000000;
-#else
-   int count = 1;
-#endif
+   // Use this to mark null values
+   const int null = -999;
+   json.nullIntValue = null;
 
-   while (count--)
+   kJSON_InitRoot(jsonHandle);
+
+   int digits[] = {0, 1, 2, 3, null, 5, 6, 7, 8, 9};
+   const char *bobsFriends[] = {"Alice", "John", "Mary", NULL, "Peter", "Paul"};
+
+   kJSON_InsertArrayInt(jsonHandle, "digits", digits, array_size(digits));
+
+   kJSON_EnterObject(jsonHandle, "people");
    {
-      kJSON_InitRoot(jsonHandle);
-
-      int digits[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-      kJSON_InsertArrayInt(jsonHandle, "digits", digits, array_size(digits));
-
-      kJSON_EnterObject(jsonHandle, "people");
+      kJSON_EnterObject(jsonHandle, "bob");
       {
-         kJSON_EnterObject(jsonHandle, "bob");
-         {
-            kJSON_InsertString(jsonHandle, "name", "Bob");
-            kJSON_InsertUnsignedNumber(jsonHandle, "age", 32);
-            kJSON_InsertBoolean(jsonHandle, "married", false);
-            kJSON_InsertString(jsonHandle, "car", "🚗");
-            kJSON_InsertNumber(jsonHandle, "balance", -300);
-         }
-         kJSON_ExitObject(jsonHandle);
+         kJSON_InsertString(jsonHandle, "name", "Bob");
+         kJSON_InsertNull(jsonHandle, "job");
+         kJSON_InsertString(jsonHandle, "bestFriend", bobsFriends[3]);
+         kJSON_InsertArrayString(jsonHandle, "friends", bobsFriends, array_size(bobsFriends));
+         kJSON_InsertUnsignedNumber(jsonHandle, "age", 32);
+         kJSON_InsertBoolean(jsonHandle, "married", false);
+         kJSON_InsertString(jsonHandle, "car", "🚗");
+         kJSON_InsertNumber(jsonHandle, "balance", -300);
       }
       kJSON_ExitObject(jsonHandle);
-
-      kJSON_ExitRoot(jsonHandle);
-
-#if !TEST
-      printf("%s\r\n", json.root);
-      printf("size: %zu\r\n", json.size);
-      printf("truncated: %d\r\n", json.truncated);
-      if (json.truncated)
-      {
-         return 1;
-      }
-#endif
-      json.tail = json.root;
-      json.size = 0;
    }
+   kJSON_ExitObject(jsonHandle);
+
+   kJSON_ExitRoot(jsonHandle);
+
+   printf("%s\r\n", json.root);
+   printf("size: %zu\r\n", json.size);
+   printf("truncated: %d\r\n", json.truncated);
+   if (json.truncated)
+   {
+      return 1;
+   }
+
+   // Reset the json object
+   json.tail = json.root;
+   json.size = 0;
+
    return 0;
 }
