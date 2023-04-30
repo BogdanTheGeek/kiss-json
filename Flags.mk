9b8b37fbdb11fea05e9fae3e3a256e8b758df283
BogdanTheGeek <bogdan.ionescu.work@gmail.com>
2023-04-30 14:20:41 +0100

    fix flags

--- Flags.mk
+++ Flags.mk
@@ -1,4 +1,4 @@
 INC := $(INC)
 INC := $(addprefix -I,$(INC))
-CFLAGS += -g -O0 -Wall -Wextra -Wmissing-declarations -Wconversion -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal -Wsuggest-attribute=const -Wunknown-warning-option -Wunused -Wuninitialized -Wno-unknown-warning-option $(INC)
+CFLAGS += -g -O0 -Wall -Wextra -Wmissing-declarations -Wconversion -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal -Wsuggest-attribute=const -Wunused -Wuninitialized -Wno-unknown-warning-option $(INC)
 
